e7be9cf3b0fd8a42ea817eb06fca90bbc7de63c5
Luis Rodriguez <lrodriguez@gowaiicorp.com>
2018-01-14 17:24:05 +0100

    refactor: rename

--- DAL/Gambon/Gambon/src/SqlServer/SqlDataReaderExensions.cs
+++ DAL/Gambon/Gambon/src/SqlServer/SqlDataReaderExensions.cs
@@ -8,8 +8,8 @@ namespace Gambon.SqlServer
 	public static class SqlDataReaderExensions
 	{
 		public static dynamic ToDynamic(this SqlDataReader dataReader) {
-			dynamic e = new ExpandoObject();
-			var result = e as IDictionary<string, object>;
+			dynamic expandoObject = new ExpandoObject();
+			var result = expandoObject as IDictionary<string, object>;
 			var values = new object[dataReader.FieldCount];
 			dataReader.GetValues(values);
 			for(var i = 0; i < values.Length; i++)
